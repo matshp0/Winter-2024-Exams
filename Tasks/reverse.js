ceb49bca77b4e21d18339fe941195f78483f3778
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 18:06:50 +0200

    Removed unnecessary arguments

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,12 +1,12 @@
 // Reverse dict, exchange keys and values
 
 const Reverse = (DATA) => {
-  const T = Object.keys(DATA, 500);
+  const T = Object.keys(DATA);
   T.forEach((_) => {
     const v1 = DATA[_];
     DATA[v1] = _;
     delete DATA[_];
-  }, 1000);
+  });
   return DATA;
 };
 
