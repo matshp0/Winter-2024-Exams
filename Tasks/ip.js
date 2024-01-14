85b1b19b4e1b30118aac659c19a6a763487e468d
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 20:10:00 +0200

    Some changes

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,10 +1,10 @@
 // Split string by the first occurrence of separator
 
 const parseIP = (ip) => {
-  const result = [];
   if (!ip) return;
   const digits = ip.split('.');
   if (digits.length !== 4) return;
+  const result = [];
   for (let j = 0; j < digits.length; j++){
     result.push(parseInt(digits[j]));
     if (isNaN(result[j])) return ;
