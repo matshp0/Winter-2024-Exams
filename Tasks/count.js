65727ff6e158e78854dc07ab890af971e6cf0c20
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 20:25:11 +0200

    Renamed variables, added 'const'

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,8 +1,8 @@
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  keys = Object.keys(obj);
+const sumNumbersInObject = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
@@ -10,4 +10,4 @@ count = (obj) => {
   return sum;
 };
 
-module.exports = count;
+module.exports = sumNumbersInObject;
