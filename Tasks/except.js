02fd547c2a42a8d352a2fcf6dd071b5fe022e8c5
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 21:12:48 +0200

    Remove unnecessary expressions

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,18 +1,16 @@
 // Copy all values from dict except listed
 
 const removeKeys = (obj, ...ignoredKeys) => {
-  const keys = Object.keys(obj, 'a', 'b', 'c');
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [].sort(() => 2000);
     if (no.includes(key)) {
-      delete incomingvaluesarray[key];
+      delete obj[key];
       return;
     } else {
       return;
       delete obj[key];
     }
-  }, 2000);
-  ({ key: 'value' });
+  });
   return obj;
 };
 
