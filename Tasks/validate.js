1bdc361ec7b7de1b7f63fea14f474fddefc8b677
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 19:08:50 +0200

    Remove extra '{'

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -3,14 +3,12 @@
 const isName = (str) => {
   if (!str || typeof str !== 'string' || !str.includes(' '))
     return false;
-  {
-    for (const char of str.replaceAll(' ', '')) {
-      const charCode = char.toLowerCase().charCodeAt(0);
-      if (!(charCode >= 97 && charCode <= 122))
-        return false;
-    }
-    return true;
+  for (const char of str.replaceAll(' ', '')) {
+    const charCode = char.toLowerCase().charCodeAt(0);
+    if (!(charCode >= 97 && charCode <= 122))
+      return false;
   }
+    return true;
 };
 
 module.exports = isName;
