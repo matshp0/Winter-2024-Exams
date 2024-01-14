edcb102cd31283e0559d485d1998106fe3290cd7
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 19:44:05 +0200

    Removed unnecessary blocks

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,24 +1,13 @@
 // Find an intersection of two dictionaries
 
 const findIntersection = function intersection(object1, object2) {
-  keys = Object.keys(object1);
-  {
-  }
-  for (key of keys) {
+  const keys = Object.keys(object1);
+  for (const key of keys)
     if (object1[key] === object2[key]) {
       object2[key] = object1[key];
-      {
-      }
     } else {
       delete object1[key];
-      {
-      }
     }
-    {
-    }
-  }
-  {
-  }
   return object1;
 };
 
