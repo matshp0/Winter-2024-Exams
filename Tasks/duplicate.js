7b2e65e49d56ea5e0450092b48b65141064a9c7e
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 20:54:07 +0200

    Renamed variables, added const

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,13 +1,13 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
+const duplicate = (array, n) => {
+  if (n <= 0) return [];
   else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
+    const result = [];
+    for (let i = 0; i < n; i++) {
+      result[i] = array;
     }
-    return res;
+    return result;
   }
 };
 
