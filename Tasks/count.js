1c8b9dae25d66acd349f4e5eea484ba846350b26
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 20:26:49 +0200

    Remove forEach with for of

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,10 +3,10 @@
 const sumNumbersInObject = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
+  }
   return sum;
 };
 
