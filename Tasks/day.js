e6f677d97a9aad18bf40f7de11642ce63a8b3f1e
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 20:38:59 +0200

    Removed '{}'

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,11 +3,9 @@
 let DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDays = (str) => {
-  for (let i = 0; i < DAYS.length; i++) {
-    if (str.startsWith(DAYS[i])) {
+  for (let i = 0; i < DAYS.length; i++)
+    if (str.startsWith(DAYS[i]))
       return i + 1;
-    }
-  }
   return -1;
 };
 
