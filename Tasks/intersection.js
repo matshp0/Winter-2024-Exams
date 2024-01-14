81311f92ab2e3ea95ccbe78dde9a35d76f105e59
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 20:23:39 +0200

    Removed unnecessary expressions

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,11 +1,9 @@
 // Find an intersection of two dictionaries
 
-const findIntersection = function intersection(object1, object2) {
+const findIntersection = (object1, object2) => {
   const keys = Object.keys(object1);
   for (const key of keys)
-    if (object1[key] === object2[key])
-      object2[key] = object1[key];
-    else
+    if (object1[key] !== object2[key])
       delete object1[key];
   return object1;
 };
