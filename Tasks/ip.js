89ae8bdb5c89edb4c1135aae3797fa9293bf7da3
m.shpyruk.im34@kpi.ua <m.shpyruk.im34@kpi.ua>
2024-01-14 20:06:35 +0200

    Removed unnecessary 'else'

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,15 +3,13 @@
 const parseIP = (ip) => {
   const result = [];
   if (ip === '') return;
-  else {
-    const digits = ip.split('.');
-    if (digits.length != 4) return;
-    j = 0;
-    for (const b of digits) {
-      result[j] = parseInt(b);
-      if (isNaN(result[j])) return;
-      j++;
-    }
+  const digits = ip.split('.');
+  if (digits.length !== 4) return;
+  j = 0;
+  for (const b of digits) {
+    result[j] = parseInt(b);
+    if (isNaN(result[j])) return;
+    j++;
   }
   return result;
 };
